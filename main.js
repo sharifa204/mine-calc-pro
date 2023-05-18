d5016d46b43e43a14eea48ae88eb26da3e05181c
sharifa ahmadi <sharifaahmadi204@gmail.com>
2023-05-18 12:19:53 +0430

    Added clear and equal button

--- main.js
+++ main.js
@@ -7,4 +7,6 @@
   // targiting the screen has class screen
   let screen = document.querySelector(".screen");
   let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector(".btn-clear");
+  let equal = document.querySelector(".btn-equal");
 })();
